ed3f3688dba9fc0779be20bf77d403802cde13d1
donkeystep <voronovsd@gmail.com>
2019-10-05 00:56:53 +0300

    local data init draft - fix merge conflicts

--- src/main/java/org/eventbot/callback/ListGroupsByCreatorCallbackAction.kt
+++ src/main/java/org/eventbot/callback/ListGroupsByCreatorCallbackAction.kt
@@ -2,18 +2,18 @@ package org.eventbot.callback
 
 import org.eventbot.CallbackAction
 import org.eventbot.model.UserInfo
-import org.eventbot.repository.TeamRepository
+import org.eventbot.repository.GroupRepository
 import org.eventbot.service.MessageService
 import org.springframework.stereotype.Component
 
 @Component
 class ListGroupsByCreatorCallbackAction(
-        private val teamRepository: TeamRepository,
+        private val GroupRepository: GroupRepository,
         private val messageService: MessageService
 ) : CallbackAction {
 
     override fun doAction(context: Map<CallbackParams, Any>): String? {
-        val listOfGroups = teamRepository.findByCreator(context[CallbackParams.USER_INFO] as UserInfo)
+        val listOfGroups = GroupRepository.findByCreator(context[CallbackParams.USER_INFO] as UserInfo)
                 .map { it.token } // TODO: replace by names
                 .joinToString("\n")
 
